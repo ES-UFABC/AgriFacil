46880d80c07522bc3fa16905e3f56cd0c295b6da
bnfranca <101370053+bnfranca@users.noreply.github.com>
2022-05-09 22:23:59 -0300

    Add files via upload
    Criação do arquivo do histórico de pedido

---
+++ api/order.py
@@ -0,0 +1,14 @@
+from flask import render_template, session, Blueprint
+from crud import order 
+
+app = Blueprint("order", "app")
+
+@app.route('/order-history', methods=['GET', 'POST'])
+def order_history():
+    order_history = order.get_order_history(session['cpf'])
+    return render_template("order_history.html", order_history = order_history)
+
+@app.route('/sell-history', methods=['GET', 'POST'])
+def sell_history():
+    sell_history = order.get_sell_history(session['id'])
+    return render_template("sell_history.html", sell_history = sell_history)
